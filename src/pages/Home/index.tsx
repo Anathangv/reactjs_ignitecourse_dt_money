cb9369a132707cefe52e5cc194559df4836011d8
Anathan <anathan.gv@gmail.com>
2023-01-20 09:45:17 -0300

    feat: clean code and note for new features

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,33 +4,11 @@ import { Table } from './components/Table'
 import { HomeContainer } from './styles'
 
 /*
-Technology
- - react
-  - useCallback
-  - memo
- - styled-components  
-  - useTheme
-  - ThemeProvider
-  - createGlobalStyle
- - phosphor icons
- - radix-ui
-  - react-dialog
-  - react-radio-group
- - react-hook-form
-  - uncontrolled
-  - controlled
-    - Controller
-  - hookform/resolvers
-  - zod
- - Intl - formatter
- - json-server
-  - full-text search
-  - query params, sort and order
- - custom hook
- - axios
- - use-context-selector / scheduler - watch a especif information and not to others in this context
-
+TODO
+[] - include edit and delete functionality
+[] - CRUD for category
 */
+
 export function Home() {
   return (
     <>
